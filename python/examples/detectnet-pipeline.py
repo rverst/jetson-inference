4ce92a39b87dccc6d4389beb6c4adfefe8153014
Robert Verst <robert.verst@tobit.com>
2020-03-23 14:48:41 +0100

    Rename camera->pipeline

--- python/examples/detectnet-pipeline.py
+++ python/examples/detectnet-pipeline.py
@@ -15,9 +15,9 @@ parser.add_argument("--network", type=str, default="facenet", help="pre-trained 
 parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
 parser.add_argument("--threshold", type=float, default=0.8, help="minimum detection threshold to use (default is 0.8)")
 parser.add_argument("--pipeline", type=str, default="", help="gStreamer pipeline string")
-parser.add_argument("--width", type=int, default=1280, help="desired width of camera stream (default is 1280 pixels)")
-parser.add_argument("--height", type=int, default=720, help="desired height of camera stream (default is 720 pixels)")
-parser.add_argument("--depth", type=int, default=12, help="desired pixel depth of camera stream (default is 12)")
+parser.add_argument("--width", type=int, default=1280, help="desired width of pipeline stream (default is 1280 pixels)")
+parser.add_argument("--height", type=int, default=720, help="desired height of pipeline stream (default is 720 pixels)")
+parser.add_argument("--depth", type=int, default=12, help="desired pixel depth of pipeline stream (default is 12)")
 
 try:
 	opt = parser.parse_known_args()[0]
@@ -29,14 +29,14 @@ except:
 # load the object detection network
 net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
 
-# create the camera and display
-camera = jetson.utils.gstPipeline(opt.pipeline, opt.width, opt.height, opt.depth)
+# create the pipeline and display
+pipeline = jetson.utils.gstPipeline(opt.pipeline, opt.width, opt.height, opt.depth)
 display = jetson.utils.glDisplay()
 
 # process frames until user exits
 while display.IsOpen():
 	# capture the image
-	img, width, height = camera.CaptureRGBA()
+	img, width, height = pipeline.CaptureRGBA()
 
 	# detect objects in the image (with overlay)
 	detections = net.Detect(img, width, height, opt.overlay)
